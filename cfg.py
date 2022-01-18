5fcad9c18c36f90229c86b9099b0fa55e07a6290
HassanTSF <hassantsf.acd@gmail.com>
2022-01-18 21:40:57 +0330

    Replace list with set in CFG

--- cfg.py
+++ cfg.py
@@ -1,25 +1,27 @@
 class CFG:
-    def __init__(self, start_variable='S', rules={}):
+    def __init__(self, start_variable='S', rules=None):
+        if rules is None:
+            rules = {}
         self.start_variable = start_variable
-        self.unused_variables = []
+        self.unused_variables = set()
         self.init_variables()
-        self.variables = list(self.unused_variables)
+        self.variables = set(self.unused_variables)
         self.rules = rules
 
     def init_variables(self):
         for i in range(65, 91):
-            self.unused_variables.append(chr(i))
-        self.unused_variables.append('S0')
+            self.unused_variables.add(chr(i))
+        self.unused_variables.add('S0')
 
     def add_rule(self, variable, rhs):
         if variable in self.rules:
-            self.rules[variable].append(rhs)
+            self.rules[variable].add(rhs)
         else:
             self.unused_variables.remove(variable)
-            self.rules[variable] = [rhs]
+            self.rules[variable] = {rhs}
 
     def remove_variable(self, variable, rhs):
         self.rules[variable].remove(rhs)
 
     def extend_rule(self, src, dest):
-        self.rules[src] += self.rules[dest]
+        self.rules[src] = self.rules[src].union(self.rules[dest])
